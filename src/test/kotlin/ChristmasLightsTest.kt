7f746c30526df076d751f2c7042049bf7b1a6f44
Daniel Bellón <daniel.bellon@payulatam.com>
2020-07-27 19:23:29 -0500

    fix turnOn method in order to turnOn a range of lights

--- src/test/kotlin/ChristmasLightsTest.kt
+++ src/test/kotlin/ChristmasLightsTest.kt
@@ -15,7 +15,7 @@ class ChristmasLightsTest {
     @Test
     fun `One light is on when range is one point`() {
         val cl = ChristmasLights(1000, 1000)
-        cl.turnOn((0 to 0), (0 to 1))
+        cl.turnOn((0 to 0), (0 to 0))
         val counter = cl.countLightsOn()
         assertEquals(1, counter)
     }
@@ -23,9 +23,18 @@ class ChristmasLightsTest {
     @Test
     fun `One light is on when turning on the same point`() {
         val cl = ChristmasLights(1000, 1000)
-        cl.turnOn((0 to 0), (0 to 1))
-        cl.turnOn((0 to 0), (0 to 1))
+        cl.turnOn((0 to 0), (0 to 0))
+        cl.turnOn((0 to 0), (0 to 0))
         val counter = cl.countLightsOn()
         assertEquals(1, counter)
+    }
+
+    @Test
+    fun `lightsOn should be 2 if the coordinates range have 2 lights to be turned on`() {
+
+        val cl = ChristmasLights(1000, 1000)
+        cl.turnOn((0 to 0), (0 to 1))
+        val lightsOnCount = cl.countLightsOn();
+        assertEquals(2, lightsOnCount);
     }
 }
