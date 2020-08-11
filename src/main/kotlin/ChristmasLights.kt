cd051b412e7003c9947ea39f789b5821819f311a
John Jairo Martinez <john.martinez@payulatam.com>
2020-08-11 18:46:51 -0500

    Refactor

--- src/main/kotlin/ChristmasLights.kt
+++ src/main/kotlin/ChristmasLights.kt
@@ -15,15 +15,18 @@ class ChristmasLights(private val width: Int, private val height: Int) {
     }
 
     private fun validateInput(start: Pair<Int, Int>, finish: Pair<Int, Int>) {
-        if (start.first < 0 || finish.first < 0 || start.second < 0 || finish.second < 0) {
+        if (isOutOfRange(start) || isOutOfRange(finish) || isInvalidRectangle(start, finish)) {
             throw BadInputException()
         }
-        if (start.first >= width || finish.first >= width || start.second >= height || finish.second >= height) {
-            throw BadInputException()
-        }
+    }
 
-        if(start.first > finish.first || start.second > finish.second)
-            throw BadInputException()
+    private fun isInvalidRectangle(
+        start: Pair<Int, Int>,
+        finish: Pair<Int, Int>
+    ) = start.first > finish.first || start.second > finish.second
+
+    private fun isOutOfRange(start: Pair<Int, Int>): Boolean {
+        return start.first < 0 || start.first >= width || start.second < 0 || start.second >= height
     }
 
     fun turnOff(start: Pair<Int, Int>, finish: Pair<Int, Int>) {
@@ -40,6 +43,14 @@ class ChristmasLights(private val width: Int, private val height: Int) {
         stateFunction: ((Int) -> Int)
     ) {
         validateInput(start, finish)
+        executeChangeState(start, finish, stateFunction)
+    }
+
+    private fun executeChangeState(
+        start: Pair<Int, Int>,
+        finish: Pair<Int, Int>,
+        stateFunction: (Int) -> Int
+    ) {
         val rows = start.first..finish.first
         val columns = start.second..finish.second
 
